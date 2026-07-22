3bd1dfe5aa1831f74d1396fabc8750d35596a147
Andri-Almengor <120224177+Andri-Almengor@users.noreply.github.com>
2026-07-22 14:17:25 -0600

    Carga el reinicio de estado de Lenel

--- template/assets/runtime/__init__.py
+++ template/assets/runtime/__init__.py
@@ -4,10 +4,12 @@ from assets.runtime import dms_console_session_guard as _console_session_guard
 from assets.runtime import dms_lenel_control_guard as _lenel_control_guard
 from assets.runtime import dms_lenel_fast_guard as _lenel_fast_guard
 from assets.runtime import dms_scan_queue_guard as _scan_queue_guard
+from assets.runtime import dms_lenel_state_guard as _lenel_state_guard
 
 __all__ = [
     "_console_session_guard",
     "_lenel_control_guard",
     "_lenel_fast_guard",
     "_scan_queue_guard",
+    "_lenel_state_guard",
 ]
