41814d8a69ba35537a9f2d13a8f742293995d5b4
Andri-Almengor <120224177+Andri-Almengor@users.noreply.github.com>
2026-07-22 14:59:17 -0600

    Activa ajustes de velocidad del escritor universal

--- template/assets/runtime/__init__.py
+++ template/assets/runtime/__init__.py
@@ -3,11 +3,13 @@
 from assets.runtime import dms_console_session_guard as _console_session_guard
 from assets.runtime import dms_scan_queue_guard as _scan_queue_guard
 from assets.runtime import dms_universal_form_guard as _universal_form_guard
+from assets.runtime import dms_writer_speed_guard as _writer_speed_guard
 from assets.runtime import dms_editor_profile_guard as _editor_profile_guard
 
 __all__ = [
     "_console_session_guard",
     "_scan_queue_guard",
     "_universal_form_guard",
+    "_writer_speed_guard",
     "_editor_profile_guard",
 ]
